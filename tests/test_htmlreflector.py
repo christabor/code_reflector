1fc4e44ccadc1d9d803786596ee55a256c8036b1
Chris Tabor <dxdstudio@gmail.com>
2015-07-11 21:26:52 -0700

    Moar tests

--- tests/test_htmlreflector.py
+++ tests/test_htmlreflector.py
@@ -34,3 +34,38 @@ class SelectorOutputTestCase(unittest.TestCase):
         expected = ('<div class="foo"><div class="bar"><div class="bim">'
                     '</div></div></div>')
         self.assertEqual(res, expected)
+
+    def test_compound_class_id(self):
+        self._setup()
+        res = self.ref.process_string('.foo#bar {}').extract().make_html(
+            save_as_string=True)
+        expected = ('<div class="foo" id="bar"></div>')
+        self.assertEqual(res, expected)
+
+    def test_compound_id_class(self):
+        self._setup()
+        res = self.ref.process_string('#foo.bar {}').extract().make_html(
+            save_as_string=True)
+        expected = ('<div class="foo" id="bar"></div>')
+        self.assertEqual(res, expected)
+
+    def test_nested_simple_class(self):
+        self._setup()
+        res = self.ref.process_string('.foo>.bar {}').extract().make_html(
+            save_as_string=True)
+        expected = ('<div class="foo"><div class="bar"></div></div>')
+        self.assertEqual(res, expected)
+
+    def test_nested_simple_id(self):
+        self._setup()
+        res = self.ref.process_string('#foo>#bar {}').extract().make_html(
+            save_as_string=True)
+        expected = ('<div id="foo"><div id="bar"></div></div>')
+        self.assertEqual(res, expected)
+
+    def test_nested_simple_id_spaces(self):
+        self._setup()
+        res = self.ref.process_string('#foo > #bar {}').extract().make_html(
+            save_as_string=True)
+        expected = ('<div id="foo"><div id="bar"></div></div>')
+        self.assertEqual(res, expected)
