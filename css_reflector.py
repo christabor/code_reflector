d23279cea1da34d89a681e88fe37b3730eb9139b
Chris Tabor <dxdstudio@gmail.com>
2015-07-12 10:11:04 -0700

    CSS Reflector: - Make type signatures and methods consistent with html reflector - Add new tests - Fix a bug with single root nodes

--- css_reflector.py
+++ css_reflector.py
@@ -13,14 +13,13 @@ class CSSReflector(Reflector):
 
     invalid_tags = ['body', 'head', 'title', 'meta']
 
-    def __init__(self, filename, nested=True):
+    def __init__(self, nested=True, newlines_and_spaces=False):
+        self.newlines_and_spaces = newlines_and_spaces
         self.nested = nested
         if nested:
             self.selectors = set()
         else:
             self.selectors = {'classes': set(), 'ids': set()}
-        with open(filename) as _file:
-            self.doc = Pq(_file.read())
 
     def __str__(self):
         ppr(self.selectors)
@@ -47,6 +46,9 @@ class CSSReflector(Reflector):
         selector += '{}{}'.format(id, classes)
         children = Pq(el).children()
         if self._is_root_body_node(el):
+            # Add for single nodes only
+            if not children:
+                self.selectors.add(selector)
             # Build nested css by traversing all child nodes and getting
             # their attributes.
             while children:
@@ -87,28 +89,47 @@ class CSSReflector(Reflector):
         out = ''
         if self.nested:
             for sel in self.selectors:
-                out += '{} '.format(sel) + '{}\n'
+                out += '{} '.format(sel) + '{}'
+                if self.newlines_and_spaces:
+                    out += '\n'
         else:
             for id in self.selectors['ids']:
-                out += '#{} '.format(id.strip()) + '{}\n'
+                out += '#{} '.format(id.strip()) + '{}'
+                if self.newlines_and_spaces:
+                    out += '\n'
             for _class in self.selectors['classes']:
-                out += '.{} '.format(_class.strip()) + '{}\n'
+                out += '.{} '.format(_class.strip()) + '{}'
+                if self.newlines_and_spaces:
+                    out += '\n'
         if save_as_string:
             return out
+        self._write(output, out)
+
+    def _write(self, output, out):
         if not output.endswith('.css'):
             raise ValueError('{} if is not a valid css file.'.format(output))
         with open(output, 'wb+') as newfile:
             newfile.write(out)
 
-    def process(self):
+    def _process(self):
         if self.nested:
             self.doc.find('*').each(self._add_nested)
         else:
             self.doc.find('*').each(self._add)
+
+    def process_string(self, html_string):
+        """Parse html with pyquery."""
+        self.doc = Pq(html_string)
+        self._process()
+        return self
+
+    def process(self, filename):
+        with open(filename) as _file:
+            self.doc = Pq(_file.read())
+        self._process()
         return self
 
 
 if DEBUG:
-    hreflector = CSSReflector('test.html')
-    hreflector.process()
-    hreflector.make_stylesheet('output-test.css')
+    hreflector = CSSReflector(newlines_and_spaces=True)
+    hreflector.process('test.html').make_stylesheet('output-test.css')
--- tests/test_cssreflector.py
+++ tests/test_cssreflector.py
@@ -10,3 +10,28 @@ class SelectorOutputTestCase(unittest.TestCase):
 
     def _setup(self):
         self.ref = css_reflector.CSSReflector()
+
+    def _wrap(self, html):
+        return '<html><body>{}</body></html>'.format(html)
+
+    def test_single_class(self):
+        self._setup()
+        res = self.ref.process_string(self._wrap(
+            '<div class="foo"></div>')).make_stylesheet(
+                save_as_string=True)
+        self.assertEqual(res, '.foo {}')
+
+    def test_single_id(self):
+        self._setup()
+        res = self.ref.process_string(self._wrap(
+            '<div id="foo"></div>')).make_stylesheet(
+                save_as_string=True)
+        self.assertEqual(res, '#foo {}')
+
+    def test_nested_id(self):
+        self._setup()
+        res = self.ref.process_string(self._wrap(
+            '<div id="foo"><div id="bar"><div id="bim">'
+            '</div></div></div>')).make_stylesheet(
+                save_as_string=True)
+        self.assertEqual(res, '#foo #bar #bim {}#foo #bar {}')
